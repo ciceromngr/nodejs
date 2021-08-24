e326531e63c56cc0cae883f10eaf8784fcd26465
Cicero Romao <cicerotransformice@gmail.com>
2021-08-24 14:07:28 -0300

    Subindo Middlewares

--- missao_node/src/server.ts
+++ missao_node/src/server.ts
@@ -1,12 +1,25 @@
 import "reflect-metadata"
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import './database'
+import 'express-async-errors'
 
 import { router } from "./routes"
 
 const PORT = 8080
 const app = express()
 app.use(express.json())
+
 app.use(router)
+
+// Tratativa de Error 
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+
+    if (err instanceof Error) return res.status(400).json({ error: err.message })
+
+    return res.status(500).json({
+        message: "Error Não Tratado",
+        error: 'Internal Server Error'
+    })
+})
 
 app.listen(PORT, () => console.log('Server is Running!'))
